8ecc607838f2735e58d598cfc06707405c1b6091
이진호 <spris1513@gmail.com>
2021-10-31 11:24:24 +0900

    회원가입 입력값 추출  - 서버 작업 시작 예정

--- app/src/main/java/com/neppplus/colosseum_211024/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_211024/SignUpActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_211024.databinding.ActivitySignUpBinding
+import com.neppplus.colosseum_211024.utils.ServerUtil
 
 class SignUpActivity : BaseActivity() {
 
@@ -15,6 +16,15 @@ class SignUpActivity : BaseActivity() {
         setValues()
     }
     override fun setupEvents() {
+
+        binding.okBtn.setOnClickListener {
+            val inputEmail = binding.emailEdt.text.toString()
+            val inputPw = binding.pwEdt.text.toString()
+            val inputNickname = binding.nicknameEdt.text.toString()
+
+//            입력 데이터를 > 서버에 회원가입 기능에 요청 > ServerUtil 함수 활용 > 함수가 아직 없으니 추가로 만들자
+//            ServerUtil.
+        }
 
     }
 
