f8a30df43dca3f68bb38e25e1d3f5cf5fea909a6
이진호 <spris1513@gmail.com>
2021-10-31 15:10:19 +0900

    저장된 토큰 조회 기능추가

--- app/src/main/java/com/neppplus/colosseum_211024/utils/ContextUtil.kt
+++ app/src/main/java/com/neppplus/colosseum_211024/utils/ContextUtil.kt
@@ -22,5 +22,12 @@ class ContextUtil {
 
         // getter - 토큰 조회 기능. LOAD
 
+        fun getToken ( context:Context ) : String {
+//            메모장을 불러내자.
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+//            불러낸 메모장에서 token 값을 찾아서 리턴(결과로 지정)
+            return pref.getString(TOKEN,"")!!  //getString 2번째 재료는 아무것도없을때 결과값
+        }
+
     }
 }
