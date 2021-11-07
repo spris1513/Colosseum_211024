b753a71a463d532ed75fb340c3b7172a9a3b7d52
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-11-07 16:20:05 +0900

    댓글 상세 정보 파싱 준비  - 표현할 UI만 우선 작업

--- app/src/main/java/com/neppplus/colosseum_211024/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/colosseum_211024/datas/ReplyData.kt
@@ -2,5 +2,7 @@ package com.neppplus.colosseum_211024.datas
 
 class ReplyData(var id : Int, var content : String) {
 
+//    var user : UserData 댓글 적은 사람 정보 > 하위 데이터로.
+
     constructor() : this(0,"내용없음")
 }
