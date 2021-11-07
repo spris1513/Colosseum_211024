aed434411f886d28f85fbe0da00f91d4db2af369
이진호 <spris1513@gmail.com>
2021-11-07 12:26:12 +0900

    토큰 제거, 로그아웃처리

--- app/src/main/java/com/neppplus/colosseum_211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_211024/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.neppplus.colosseum_211024
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AlertDialog
@@ -8,6 +9,7 @@ import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_211024.adapters.TopicAdapter
 import com.neppplus.colosseum_211024.databinding.ActivityMainBinding
 import com.neppplus.colosseum_211024.datas.TopicData
+import com.neppplus.colosseum_211024.utils.ContextUtil
 import com.neppplus.colosseum_211024.utils.ServerUtil
 import org.json.JSONObject
 
@@ -35,7 +37,14 @@ class MainActivity : BaseActivity() {
             alert.setMessage("정말 로그아웃 하시겠습니까?")
             alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
 
-//                확인 눌리면 할 일
+//                확인 눌리면 할 일 > 로그아웃
+//                로그아웃 : 저장된 토큰값을 파기(토큰제거)
+                ContextUtil.setToken(mContext,"")
+                val myIntent = Intent(mContext,SplashActivity::class.java)
+                startActivity(myIntent)
+                finish()
+
+
 
             })
             alert.setNegativeButton("취소",null)
