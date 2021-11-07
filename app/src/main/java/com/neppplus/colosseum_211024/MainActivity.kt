89993e17bd3247ec29b1d012b9426acc21e90af7
이진호 <spris1513@gmail.com>
2021-11-07 10:53:22 +0900

    메인화면 정보를 서버에 요청하는 API추가  - 파싱작업 X

--- app/src/main/java/com/neppplus/colosseum_211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_211024/MainActivity.kt
@@ -3,12 +3,15 @@ package com.neppplus.colosseum_211024
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_211024.databinding.ActivityMainBinding
+import com.neppplus.colosseum_211024.datas.TopicData
 import com.neppplus.colosseum_211024.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding : ActivityMainBinding
+
+    val mTopicList = ArrayList<TopicData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +27,31 @@ class MainActivity : BaseActivity() {
 
 //        연습 - 내 정보 받아오기 호출 => 닉네임 파싱, 텍스트뷰에 반영영
 
-        ServerUtil.getRequestMyInfo(mContext,object:ServerUtil.JsonResponseHandler{
-            override fun onResponse(jsonObj: JSONObject) {
-                val dataObj = jsonObj.getJSONObject("data")
-                val userObj = dataObj.getJSONObject("user")
-                val nickname = userObj.getString("nick_name")
+//        ServerUtil.getRequestMyInfo(mContext,object:ServerUtil.JsonResponseHandler{
+//            override fun onResponse(jsonObj: JSONObject) {
+//                val dataObj = jsonObj.getJSONObject("data")
+//                val userObj = dataObj.getJSONObject("user")
+//                val nickname = userObj.getString("nick_name")
+//
+//                binding.nicknameTxt.text = nickname
+//            }
+//
+//        })
 
-                binding.nicknameTxt.text = nickname
-            }
 
-        })
+//        /v2/main_info API 가 토론 주제 목록을 내려줌
+//        서버 호출 > 파싱해서 mTopicList 를 채워주자
+        fun getTopicListFromServer(){
+
+            ServerUtil.getRequestMainInfo(mContext,object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+                }
+            })
+
+        }
+
+
     }
 
 }
